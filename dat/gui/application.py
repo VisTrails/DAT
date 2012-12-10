37957e8dd3864ddb526a3052d2579d8dcc6d31a4
Remi Rampin <remirampin@gmail.com>
2012-12-10 17:34:08 -0500

    TODO : proper VisTrails initialization code

--- dat/gui/application.py
+++ dat/gui/application.py
@@ -3,9 +3,33 @@ from PyQt4 import QtGui
 
 from dat.gui.window import MainWindow
 
+import core.requirements
+
 
 def start():
     app = QtGui.QApplication(sys.argv)
+
+    # -----
+    # We need to initialize VisTrails here, but don't want to use its
+    # QApplication, single-instance code, splash screen or usual running modes
+    # We don't want anything to appear either, we just want to embed the
+    # spreadsheet window
+    # I believe this requires changes in VisTrails
+
+    try:
+        core.requirements.check_all_vistrails_requirements()
+    except core.requirements.MissingRequirement, e:
+        QtGui.QMessageBox.critical(
+                None,
+                _("Missing requirement"),
+                str(_("VisTrails reports that a requirement is missing.\n"
+                      "This application can't continue without {required}."))
+                        .format(required=e.requirement))
+        return 1
+
+    # VistrailsApplicationSingleton#init() goes here
+
+    # -----
 
     mw = MainWindow()
     mw.setVisible(True)
