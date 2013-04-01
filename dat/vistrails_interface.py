VisTrails/DAT

commit 3df466a719128700a3a7b3c877ac7c387f1ad6bf

Stores and displays the name of the cast operation
If typecasting occurred, the name of the operation that was used is
stored in the annotation and displayed on the overlay.

dat/vistrails_interface.py
@@ -13,7 +13,7 @@ import warnings
 
 from PyQt4 import QtCore, QtGui
 
-from dat import BaseVariableLoader, PipelineInformation, \
+from dat import BaseVariableLoader, DATRecipe, PipelineInformation, \
     RecipeParameterValue, DEFAULT_VARIABLE_NAME
 from dat.gui import translate
 
@@ -1276,17 +1276,19 @@ def add_variable_subworkflow(generator, variable, plot_ports=None):
 def add_variable_subworkflow_typecast(generator, variable, plot_ports,
                                        expected_type, typecast):
     if issubclass(variable.type.module, expected_type.module):
-        return add_variable_subworkflow(
-                generator,
-                variable.name,
-                plot_ports)
+        return (
+                add_variable_subworkflow(
+                        generator,
+                        variable.name,
+                        plot_ports),
+                RecipeParameterValue(variable=variable))
     else:
         # Load the variable from the workflow
         var_pipeline = Variable.from_pipeline(
                 generator.controller, variable.name)
 
         # Apply the operation
-        var_pipeline = typecast(
+        var_pipeline, typecast_operation = typecast(
                 generator.controller, var_pipeline,
                 variable.type, expected_type)
 
@@ -1299,7 +1301,9 @@ def add_variable_subworkflow_typecast(generator, variable, plot_ports,
                         var_pipeline._outputport_name,
                         var_output_mod,
                         var_output_port))
-            return connection_ids
+            return connection_ids, RecipeParameterValue(
+                    variable=variable,
+                    typecast=typecast_operation.name)
         else:
             return (var_pipeline._output_module, var_pipeline._outputport_name)
 
@@ -1418,18 +1422,21 @@ def create_pipeline(controller, recipe, cell_info, typecast=None):
     conn_map = dict() # param: str -> [[conn_id: int]]
 
     name_to_port = {port.name: port for port in recipe.plot.ports}
-
+    actual_parameters = {}
     for port_name, parameters in recipe.parameters.iteritems():
         plot_ports = plot_params.get(port_name, [])
         p_conns = conn_map[port_name] = []
+        actual_values = []
         for parameter in parameters:
             if parameter.type == RecipeParameterValue.VARIABLE:
-                p_conns.append(add_variable_subworkflow_typecast(
+                conns, actual_param = add_variable_subworkflow_typecast(
                         generator,
                         parameter.variable,
                         plot_ports,
                         name_to_port[port_name].type,
-                        typecast=typecast))
+                        typecast=typecast)
+                p_conns.append(conns)
+                actual_values.append(actual_param)
             else: # parameter.type == RecipeParameterValue.CONSTANT
                 desc = name_to_port[port_name].type
                 p_conns.append(add_constant_module(
@@ -1437,6 +1444,8 @@ def create_pipeline(controller, recipe, cell_info, typecast=None):
                         desc,
                         parameter.constant,
                         plot_ports))
+                actual_values.append(parameter)
+        actual_parameters[port_name] = actual_values
 
     pipeline_version = generator.perform_action()
     controller.vistrail.change_description(
@@ -1450,7 +1459,10 @@ def create_pipeline(controller, recipe, cell_info, typecast=None):
     for param, portlist in plot_params.iteritems():
         port_map[param] = [(module.id, port) for module, port in portlist]
 
-    return PipelineInformation(pipeline_version, recipe, conn_map, port_map)
+    return PipelineInformation(
+            pipeline_version,
+            DATRecipe(recipe.plot, actual_parameters),
+            conn_map, port_map)
 
 
 class UpdateError(ValueError):
@@ -1489,9 +1501,9 @@ def update_pipeline(controller, pipelineInfo, new_recipe, typecast=None):
     removed_params = []
 
     name_to_port = {port.name: port for port in new_recipe.plot.ports}
-
+    actual_parameters = {}
     for port_name in (set(old_recipe.parameters.iterkeys()) |
-                             set(new_recipe.parameters.iterkeys())):
+                      set(new_recipe.parameters.iterkeys())):
         # param -> [[conn_id]]
         old_params = dict()
         for i, param in enumerate(old_recipe.parameters.get(port_name, [])):
@@ -1501,6 +1513,7 @@ def update_pipeline(controller, pipelineInfo, new_recipe, typecast=None):
         conn_lists = conn_map.setdefault(port_name, [])
 
         # Loop on new parameters
+        actual_values = []
         for param in new_params:
             # Remove one from old_params
             old = old_params.get(param)
@@ -1519,12 +1532,14 @@ def update_pipeline(controller, pipelineInfo, new_recipe, typecast=None):
                           for mod_id, port in (
                                   pipelineInfo.port_map[port_name])]
             if param.type == RecipeParameterValue.VARIABLE:
-                conn_lists.append(add_variable_subworkflow_typecast(
+                conns, actual_param = add_variable_subworkflow_typecast(
                         generator,
                         param.variable,
                         plot_ports,
                         name_to_port[port_name].type,
-                        typecast=typecast))
+                        typecast=typecast)
+                conn_lists.append(conns)
+                actual_values.append(actual_param)
             else: #param.type == RecipeParameterValue.CONSTANT:
                 desc = name_to_port[port_name].type
                 conn_lists.append(add_constant_module(
@@ -1532,6 +1547,7 @@ def update_pipeline(controller, pipelineInfo, new_recipe, typecast=None):
                         desc,
                         param.constant,
                         plot_ports))
+                actual_values.append(param)
 
             added_params.append(port_name)
 
@@ -1551,6 +1567,8 @@ def update_pipeline(controller, pipelineInfo, new_recipe, typecast=None):
 
                 removed_params.append(port_name)
 
+        actual_parameters[port_name] = actual_values
+
     # We didn't find anything to change
     if not (added_params or removed_params):
         return pipelineInfo
@@ -1563,8 +1581,10 @@ def update_pipeline(controller, pipelineInfo, new_recipe, typecast=None):
 
     controller.change_selected_version(pipeline_version, from_root=True)
 
-    return PipelineInformation(pipeline_version, new_recipe,
-                               conn_map, pipelineInfo.port_map)
+    return PipelineInformation(
+            pipeline_version,
+            DATRecipe(new_recipe.plot, actual_parameters),
+            conn_map, pipelineInfo.port_map)
 
 
 def describe_dat_update(added_params, removed_params):

dat/__init__.py
@@ -19,11 +19,16 @@ class RecipeParameterValue(object):
     VARIABLE = 1
     CONSTANT = 2
 
-    def __init__(self, variable=None, constant=None):
+    def __init__(self, variable=None, constant=None, typecast=None):
         if variable is not None and constant is None:
             self.type = self.VARIABLE
             self.variable = variable
-        elif constant is not None and variable is None:
+            self.typecast = typecast # str
+            # This 'typecast' parameter is not really useful since it doesn't
+            # unambiguously point to a specific operation (remember: we allow
+            # overloading)
+            # It is only intended to be displayed for the user
+        elif constant is not None and variable is None and typecast is None:
             self.type = self.CONSTANT
             self.constant = constant
         else:

dat/global_data.py
@@ -64,9 +64,11 @@ class GlobalManager(object):
     def get_plot(self, package_identifier, plotname):
         """Gets a plot with the given name.
 
-        This is only used when building a recipe from a string.
+        This is used when building a recipe from a string and when the overlay
+        receives a drop (X-Vistrails/DATPlot).
         """
-        return self._plots[(package_identifier, plotname)] # Might raise KeyError
+        return self._plots[(package_identifier, plotname)]
+                # Might raise KeyError
 
     def _get_plots(self):
         return self._plots.itervalues()

dat/gui/cellcontainer.py
@@ -392,6 +392,10 @@ class DATCellContainer(QCellContainer):
                         recipe,
                         self.cellInfo,
                         typecast=self._typecast)
+                recipe = pipeline.recipe
+                new_params_it = recipe.parameters.iteritems()
+                self._parameters = {param: list(values)
+                                    for param, values in new_params_it}
                 vistraildata.created_pipeline(self.cellInfo, pipeline)
 
             # Pipeline with a different content: update it
@@ -411,6 +415,10 @@ class DATCellContainer(QCellContainer):
                             recipe,
                             self.cellInfo,
                             typecast=self._typecast)
+                recipe = pipeline.recipe
+                new_params_it = recipe.parameters.iteritems()
+                self._parameters = {param: list(values)
+                                    for param, values in new_params_it}
                 vistraildata.created_pipeline(self.cellInfo, pipeline)
 
             # Nothing changed
@@ -443,4 +451,4 @@ class DATCellContainer(QCellContainer):
                 typecasts,
                 source_descriptor, expected_descriptor,
                 self)
-        return apply_operation(controller, choice, [variable])
+        return apply_operation(controller, choice, [variable]), choice

dat/gui/overlays/variable_dropping.py
@@ -63,7 +63,8 @@ DataParameter[targeted="no"][compatible="no"] {
 
 
 class DataParameter(QtGui.QPushButton):
-    def __init__(self, overlay, port_name, pos, variable, append=False):
+    def __init__(self, overlay, port_name, pos, variable, typecast=None,
+            append=False):
         QtGui.QPushButton.__init__(self)
 
         self.setSizePolicy(QtGui.QSizePolicy.Minimum,
@@ -72,7 +73,10 @@ class DataParameter(QtGui.QPushButton):
 
         self._variable = variable
         if variable is not None:
-            self.setText(variable.name)
+            if typecast is not None:
+                self.setText("(%s) %s" % (typecast, variable.name))
+            else:
+                self.setText(variable.name)
 
             self.connect(
                     self,
@@ -157,7 +161,8 @@ class VariableDroppingOverlay(Overlay):
                 for pos, variable in enumerate(
                         self._cell._parameters.get(port.name, [])):
                     param = DataParameter(self, port.name, pos,
-                                          variable=variable.variable)
+                                          variable=variable.variable,
+                                          typecast=variable.typecast)
                     param.setProperty('compatible', compatible)
                     param.setProperty('optional', port.optional)
                     param.setProperty('targeted', 'no')
@@ -165,7 +170,8 @@ class VariableDroppingOverlay(Overlay):
                     param_panel.layout().addWidget(param)
                 if ((port.multiple_values and compatible == 'yes') or
                         not self._cell._parameters.get(port.name)):
-                    param = DataParameter(self, port.name, pos, None,
+                    param = DataParameter(self, port.name, pos,
+                                          variable=None,
                                           append=compatible == 'yes')
                     param.setProperty('compatible', compatible)
                     param.setProperty('optional', port.optional)

dat/tests/test_dat.py
@@ -1,0 +1,71 @@
+"""Tests for the dat package.
+
+"""
+
+
+import unittest
+
+from dat import DATRecipe, RecipeParameterValue
+from dat.tests import FakeObj
+
+
+class TestRecipe(unittest.TestCase):
+    def test_eq(self):
+        myvar = FakeObj(name='myvar')
+        othervar = FakeObj(name='othervar')
+        plot = FakeObj(package_identifier='tests.dat', name='My Plot')
+        plot2 = FakeObj(package_identifier='tests.dat', name='Not My Plot')
+        rec1 = DATRecipe(
+                plot,
+                dict(
+                        param1=[],
+                        param2=[RecipeParameterValue(variable=myvar)]))
+        rec2 = DATRecipe(
+                plot2,
+                dict(
+                        param1=[],
+                        param2=[RecipeParameterValue(variable=myvar)]))
+        rec3 = DATRecipe(
+                plot,
+                dict(
+                        param1=(),
+                        param2=(RecipeParameterValue(variable=myvar),)))
+        rec4 = DATRecipe(
+                plot,
+                dict(
+                        param1=[RecipeParameterValue(variable=othervar)],
+                        param2=[RecipeParameterValue(variable=myvar)]))
+        rec5 = DATRecipe(
+                plot,
+                dict(
+                        param1=[],
+                        param2=[RecipeParameterValue(constant='myvar')]))
+        rec6 = DATRecipe(
+                plot,
+                dict(
+                        param1=[],
+                        param2=[RecipeParameterValue(constant='othervar')]))
+        rec7 = DATRecipe(
+                plot,
+                dict(
+                        param1=[],
+                        param2=[RecipeParameterValue(variable=myvar,
+                                                     typecast='op1')]))
+        rec8 = DATRecipe(
+                plot,
+                dict(
+                        param1=[],
+                        param2=[RecipeParameterValue(variable=myvar,
+                                                     typecast='*')]))
+
+        self.assertTrue (rec1 == rec1)
+        self.assertTrue (rec3 == rec3)
+        self.assertTrue (rec5 == rec5)
+        self.assertFalse(rec1 == rec2)
+        self.assertTrue (rec1 == rec3)
+        self.assertFalse(rec1 == rec4)
+        self.assertFalse(rec1 == rec5)
+        self.assertFalse(rec1 == rec6)
+        self.assertTrue(rec1 == rec7)
+        self.assertTrue (rec7 == rec7)
+        self.assertTrue (rec7 == rec8)

dat/vistrail_data.py
@@ -43,7 +43,7 @@ class VistrailData(object):
     #   plot_package,PlotName;
     #       param1=v=
     #           varname1:CONN1,CONN2|
-    #           varname2:CONN3;
+    #           varname2,cast_op:CONN3;
     #       param2=c=value2
     #
     # And <portmap>:
@@ -62,6 +62,7 @@ class VistrailData(object):
     #   * CONN<M> with the id of a connection tying the plot input port to one
     #     of the parameters set to this port
     #   * value<N> is the string representation of a constant
+    #   * cast_op is the name of the variable operation used for typecasting
     #
     # Parameters which are not set are simply omitted from the list
     _RECIPE_KEY = 'dat-recipe'
@@ -92,6 +93,8 @@ class VistrailData(object):
                     value += urllib2.quote(param_val.constant, safe='')
                 else: # param_val.type == RecipeParameterValue.VARIABLE
                     value += param_val.variable.name
+                    if param_val.typecast is not None:
+                        value += ',%s' % param_val.typecast
                 value += ':' + ','.join(
                         '%d' % conn_id
                         for conn_id in conn_list)
@@ -123,12 +126,23 @@ class VistrailData(object):
                     raise ValueError
                 for val in pvals:
                     val = val.split(':')
+                    if len(val) != 2:
+                        raise ValueError
                     if t == 'c':
                         plist.append(RecipeParameterValue(
                                 constant=urllib2.unquote(val[0])))
                     else: # t == 'v':
-                        plist.append(RecipeParameterValue(
-                                variable=vistraildata.get_variable(val[0])))
+                        v = val[0].split(',')
+                        if len(v) not in (1, 2):
+                            raise ValueError
+                        variable = vistraildata.get_variable(v[0])
+                        if len(v) == 2:
+                            plist.append(RecipeParameterValue(
+                                    variable=variable,
+                                    typecast=v[1]))
+                        else:
+                            plist.append(RecipeParameterValue(
+                                    variable=variable))
                     cplist.append(read_connlist(val[1]))
                 parameters[param] = tuple(plist)
                 conn_map[param] = tuple(cplist)

commit 5b3f5d2a6bbdccfbb6f8645335365c447e645e74

Fixes a typo in update_pipeline()
This error introduced in 3df466a7 "Stores and displays the name of the
cast operation" was forgetting all parameters that update_pipeline()
didn't update.

dat/vistrails_interface.py
@@ -1523,6 +1523,7 @@ def update_pipeline(controller, pipelineInfo, new_recipe, typecast=None):
                     del old_params[param]
 
                 conn_lists.append(old_conns)
+                actual_values.append(param)
                 continue
 
             # Can't remove, meaning that there is more of this param than there